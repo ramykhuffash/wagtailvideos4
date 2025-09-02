4daa938ab2775c63c7a60be4ee4b92de82ac9e43
ramykhuffash <khuffash.ramy@gmail.com>
2025-09-02 21:17:34 +0100

    import errors

--- wagtailvideos/__init__.py
+++ wagtailvideos/__init__.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.functional import SimpleLazyObject
 
 default_app_config = 'wagtailvideos.apps.WagtailVideosApp'
-
-# Cache the model once it's loaded
-_video_model = None
 
 
 def is_modeladmin_installed():
@@ -16,38 +14,19 @@ def get_video_model_string():
     return getattr(settings, 'WAGTAILVIDEOS_VIDEO_MODEL', 'wagtailvideos.Video')
 
 
-def get_video_model():
-    global _video_model
-    
-    # Return cached model if already loaded
-    if _video_model is not None:
-        return _video_model
-    
+def _get_video_model():
+    """Internal function that actually loads the model"""
     from django.apps import apps
-    
-    # Try to get the model, but don't fail during Django startup
     model_string = get_video_model_string()
     try:
-        _video_model = apps.get_model(model_string)
-        return _video_model
+        return apps.get_model(model_string)
     except ValueError:
         raise ImproperlyConfigured("WAGTAILVIDEOS_VIDEO_MODEL must be of the form 'app_label.model_name'")
-    except LookupError as e:
-        # During Django startup, models might not be available yet
-        # Check if it's a startup issue vs a real config problem
-        if apps.ready:
-            # Apps are ready but model still not found - real error
-            raise ImproperlyConfigured(
-                "WAGTAILVIDEOS_VIDEO_MODEL refers to model '%s' that has not been installed" % model_string
-            )
-        else:
-            # Apps not ready yet, try to import the model class directly as fallback
-            try:
-                from django.utils.module_loading import import_string
-                app_label, model_name = model_string.split('.')
-                model_class_path = f"{app_label}.models.{model_name}"
-                _video_model = import_string(model_class_path)
-                return _video_model
-            except:
-                # If that fails too, return None and let it be resolved later
-                return None
+    except LookupError:
+        raise ImproperlyConfigured(
+            "WAGTAILVIDEOS_VIDEO_MODEL refers to model '%s' that has not been installed" % model_string
+        )
+
+
+# This is the key - use Django's SimpleLazyObject to defer loading
+get_video_model = SimpleLazyObject(_get_video_model)
